0790ffdf20ad71f99cb5015858b9cd3a64a01d52
Cristian Morales <cristian.morales@bnamericas.com>
2021-07-04 23:38:00 -0400

    naive bayes

--- pocovidnet/naive_bayes.py
+++ pocovidnet/naive_bayes.py
@@ -1,41 +1,49 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-from sklearn.linear_model import LogisticRegression
 import pandas as pd
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.model_selection import cross_val_score
-import numpy as np
-from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
 
-data_base = pd.read_csv("test_data/test_model_2_blur_filter.csv", sep=',')
-data_base = data_base.replace(['covid', 'pneumonia', 'regular'], [0, 1, 2])
-
-# filtering data
-dbtrain = data_base.loc[data_base['66']=='train']
-dbtest = data_base.loc[data_base['66']=='test']
-
-X_train = dbtrain.drop(dbtrain.columns[[-3,-1]], axis=1)
-Y_train = X_train.iloc[:,-1]
+#obtenido desde
+#https://www.datacamp.com/community/tutorials/naive-bayes-scikit-learn
 
 
-X_test = dbtest.drop(dbtest.columns[[-3,-1]], axis=1)
-Y_test = X_test.iloc[:,-1]
+def naive(name):
+    fileName = "test_data/"+name
+    data_base = pd.read_csv(fileName, sep=',')
+    data_base = data_base.replace(['covid', 'pneumonia', 'regular'], [0, 1, 2])
 
-#cross_val_score(RandomForestClassifier(n_estimators=5),X_train, Y_train)
+    # filtering data
+    dbtrain = data_base.loc[data_base['66']=='train']
+    dbtest = data_base.loc[data_base['66']=='test']
 
-svm = SVC(gamma='auto')
-svm.fit(X_train, Y_train)
-scoresvm =  svm.score(X_test, Y_test)
+    X_train = dbtrain.drop(dbtrain.columns[[-3,-1]], axis=1)
+    Y_train = X_train.iloc[:,-1]
 
 
-rf = RandomForestClassifier(n_estimators=40)
-rf.fit(X_train, Y_train)
-scorerf = rf.score(X_test, Y_test)
+    X_test = dbtest.drop(dbtest.columns[[-3,-1]], axis=1)
+    Y_test = X_test.iloc[:,-1]
 
-lr = LogisticRegression(solver='liblinear',multi_class='ovr')
-lr.fit(X_train, Y_train)
-scorelg = lr.score(X_test, Y_test)
+    #Create a Gaussian Classifier
+    gnb = GaussianNB()
 
-print(scoresvm,'SVM')
-print(scorerf,'RF')
-print(scorelg,'LG')
+    #Train the model using the training sets
+    gnb.fit(X_train, Y_train)
+
+    #Predict the response for test dataset
+    y_pred = gnb.predict(X_test)
+
+    # Model Accuracy, how often is the classifier correct?
+    print(name)
+    print("Accuracy:" ,metrics.accuracy_score(Y_test, y_pred))
+    print(metrics.classification_report(Y_test, y_pred))
+
+    print(metrics.confusion_matrix(Y_test, y_pred))
+
+naive("test_model_0_no_filter.csv")
+naive("test_model_1_2d_filter.csv")
+naive("test_model_2_blur_filter.csv")
+naive("test_model_3_mediablur_filter.csv")
+naive("test_model_4_laplacian_filter.csv")
+naive("test_model_5_sobel_filter.csv")
+naive("test_model_6_equalizeHist_filter.csv")
+
+
